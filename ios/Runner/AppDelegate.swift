f57f3e7f095cab14a0fa82d1c39c4d0937a350b9
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-03 21:53:59 +0300

    Remove unneeded resource files from iOS Project
    * Remove breez_liquid_sdk.h
    * Remove outdated method to enforce bundling on AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,8 +7,6 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    let dummy = dummy_method_to_enforce_bundling()
-    print(dummy)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
