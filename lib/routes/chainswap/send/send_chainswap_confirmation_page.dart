ad1cb719ed50987ad695282be7196bdc63bc1bf3
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-11 22:33:13 +0300

    Integrate Send Chain Swap

---
+++ lib/routes/chainswap/send/send_chainswap_confirmation_page.dart
@@ -0,0 +1,125 @@
+import 'package:breez_liquid/breez_liquid.dart';
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:l_breez/bloc/account/account_bloc.dart';
+import 'package:l_breez/bloc/chainswap/chainswap_bloc.dart';
+import 'package:l_breez/routes/chainswap/send/fee/fee_breakdown/fee_breakdown.dart';
+import 'package:l_breez/routes/chainswap/send/fee/fee_option.dart';
+import 'package:l_breez/routes/chainswap/send/send_chainswap_button.dart';
+import 'package:l_breez/widgets/loader.dart';
+
+class SendChainSwapConfirmationPage extends StatefulWidget {
+  final int amountSat;
+  final String onchainRecipientAddress;
+  final bool isMaxValue;
+
+  const SendChainSwapConfirmationPage({
+    super.key,
+    required this.amountSat,
+    required this.onchainRecipientAddress,
+    required this.isMaxValue,
+  });
+
+  @override
+  State<SendChainSwapConfirmationPage> createState() => _SendChainSwapConfirmationPageState();
+}
+
+class _SendChainSwapConfirmationPageState extends State<SendChainSwapConfirmationPage> {
+  bool isAffordable = false;
+  PreparePayOnchainResponse? feeOption;
+
+  late Future<PreparePayOnchainResponse> _preparePayOnchainResponseFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _preparePayOnchainResponse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = context.texts();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(texts.csv_exporter_fee),
+      ),
+      body: FutureBuilder(
+        future: _preparePayOnchainResponseFuture,
+        builder: (context, snapshot) {
+          if (snapshot.error != null) {
+            return _ErrorMessage(
+              message: (snapshot.error is PaymentError_InsufficientFunds)
+                  ? texts.reverse_swap_confirmation_error_funds_fee
+                  : texts.reverse_swap_confirmation_error_fetch_fee,
+            );
+          }
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(child: Loader());
+          }
+
+          if (isAffordable) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 40.0),
+              child: FeeBreakdown(feeOption: snapshot.data!),
+            );
+          } else {
+            return _ErrorMessage(message: texts.reverse_swap_confirmation_error_funds_fee);
+          }
+        },
+      ),
+      bottomNavigationBar: (isAffordable)
+          ? SafeArea(
+              child: SendChainSwapButton(
+                recipientAddress: widget.onchainRecipientAddress,
+                preparePayOnchainResponse: feeOption!,
+              ),
+            )
+          : null,
+    );
+  }
+
+  void _preparePayOnchainResponse() {
+    final chainSwapBloc = context.read<ChainSwapBloc>();
+    final preparePayOnchainRequest = PreparePayOnchainRequest(
+      receiverAmountSat: BigInt.from(widget.amountSat),
+    );
+    _preparePayOnchainResponseFuture = chainSwapBloc.preparePayOnchain(
+      req: preparePayOnchainRequest,
+    );
+    _preparePayOnchainResponseFuture.then((feeOption) {
+      final account = context.read<AccountBloc>().state;
+      setState(() {
+        this.feeOption = feeOption;
+        isAffordable = feeOption.isAffordable(balance: account.balance);
+      });
+    }, onError: (error, stackTrace) {
+      setState(() {
+        isAffordable = false;
+        feeOption = null;
+      });
+    });
+  }
+}
+
+class _ErrorMessage extends StatelessWidget {
+  final String message;
+
+  const _ErrorMessage({
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          message,
+          textAlign: TextAlign.center,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/chainswap/send/send_chainswap_page.dart
@@ -0,0 +1,87 @@
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
+import 'package:l_breez/bloc/chainswap/chainswap_bloc.dart';
+import 'package:l_breez/bloc/currency/currency_bloc.dart';
+import 'package:l_breez/routes/chainswap/send/send_chainswap_form_page.dart';
+import 'package:l_breez/utils/exceptions.dart';
+import 'package:l_breez/widgets/back_button.dart' as back_button;
+import 'package:l_breez/widgets/loader.dart';
+
+class SendChainSwapPage extends StatefulWidget {
+  final BitcoinAddressData? btcAddressData;
+  const SendChainSwapPage({super.key, required this.btcAddressData});
+
+  @override
+  State<SendChainSwapPage> createState() => _SendChainSwapPageState();
+}
+
+class _SendChainSwapPageState extends State<SendChainSwapPage> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  Future<OnchainPaymentLimitsResponse>? _onchainPaymentLimitsFuture;
+  @override
+  void initState() {
+    super.initState();
+    _fetchOnchainLimits();
+  }
+
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _fetchOnchainLimits();
+    }
+  }
+
+  Future _fetchOnchainLimits() async {
+    final chainSwapBloc = context.read<ChainSwapBloc>();
+    setState(() {
+      _onchainPaymentLimitsFuture = chainSwapBloc.fetchOnchainLimits();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final texts = context.texts();
+    final themeData = Theme.of(context);
+
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: AppBar(
+        leading: const back_button.BackButton(),
+        title: Text(texts.bottom_action_bar_send_btc_address),
+      ),
+      body: FutureBuilder<OnchainPaymentLimitsResponse>(
+        future: _onchainPaymentLimitsFuture,
+        builder: (BuildContext context, AsyncSnapshot<OnchainPaymentLimitsResponse> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+                child: Text(
+                  texts.reverse_swap_upstream_generic_error_message(
+                    extractExceptionMessage(snapshot.error!, texts),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            );
+          }
+          if (snapshot.connectionState != ConnectionState.done && !snapshot.hasData) {
+            return Center(
+              child: Loader(
+                color: themeData.primaryColor.withOpacity(0.5),
+              ),
+            );
+          }
+
+          return SendChainSwapFormPage(
+            bitcoinCurrency: context.read<CurrencyBloc>().state.bitcoinCurrency,
+            btcAddressData: widget.btcAddressData,
+            paymentLimits: snapshot.data!,
+          );
+        },
+      ),
+    );
+  }
+}
