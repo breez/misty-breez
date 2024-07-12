6ae28f3fd1f2930cbafcb62743d075df426a0d60
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-12 15:43:59 +0300

    Remove unused BlocBuilder extension

--- lib/bloc/ext/block_builder_extensions.dart
+++ lib/bloc/ext/block_builder_extensions.dart
@@ -22,27 +22,3 @@ class BlocBuilder2<Bloc1 extends BlocBase<State1>, State1, Bloc2 extends BlocBas
     });
   }
 }
-
-typedef BlocWidgetBuilder3<State1, State2, State3> = Widget Function(
-    BuildContext context, State1 state1, State2 state2, State3 state3);
-
-class BlocBuilder3<Bloc1 extends BlocBase<State1>, State1, Bloc2 extends BlocBase<State2>, State2,
-    Bloc3 extends BlocBase<State3>, State3> extends StatelessWidget {
-  final BlocWidgetBuilder3<State1, State2, State3> builder;
-
-  const BlocBuilder3({
-    super.key,
-    required this.builder,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<Bloc1, State1>(builder: (context, state1) {
-      return BlocBuilder<Bloc2, State2>(builder: (context, state2) {
-        return BlocBuilder<Bloc3, State3>(builder: (context, state3) {
-          return builder(context, state1, state2, state3);
-        });
-      });
-    });
-  }
-}
