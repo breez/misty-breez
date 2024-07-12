944b945bc739dab4239d93d29230fb506365d989
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-13 01:59:38 +0300

    Use liquid mainnet mempool instance on blockchain explorer utils

--- lib/utils/blockchain_explorer_utils.dart
+++ lib/utils/blockchain_explorer_utils.dart
@@ -1,10 +1,11 @@
 // TODO: Liquid - This file is unused - Re-add for swap tx's after input parser is implemented
 class BlockChainExplorerUtils {
-  String formatTransactionUrl({required String txid, String mempoolInstance = "https://mempool.space/"}) {
+  String formatTransactionUrl(
+      {required String txid, String mempoolInstance = "https://liquid.fra.mempool.space/"}) {
     return "$mempoolInstance/tx/$txid";
   }
 
-  String formatRecommendedFeesUrl({String mempoolInstance = "https://mempool.space/"}) {
+  String formatRecommendedFeesUrl({String mempoolInstance = "https://liquid.fra.mempool.space/"}) {
     return "$mempoolInstance/api/v1/fees/recommended";
   }
 }
