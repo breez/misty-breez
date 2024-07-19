d7965409db781ec1955bf7d36711355590e7d990
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-07-19 12:11:26 +0300

    Use recommended fees when calculating claim tx feerates for send chain swap fee estimates (#66)
    Integrate recommendedFees & display economy, regular, priority fee options

--- lib/cubit/chain_swap/chain_swap_cubit.dart
+++ lib/cubit/chain_swap/chain_swap_cubit.dart
@@ -1,10 +1,13 @@
 library chain_swap_cubit;
 
 import 'package:breez_sdk_liquid/breez_sdk_liquid.dart';
+import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
 import 'package:l_breez/cubit/chain_swap/chain_swap_state.dart';
 import 'package:l_breez/cubit/model/src/payment/payment_error.dart';
+import 'package:l_breez/routes/chainswap/send/fee/fee_option.dart';
+import 'package:l_breez/utils/exceptions.dart';
 
 export 'chain_swap_state.dart';
 
@@ -15,6 +18,10 @@ class ChainSwapCubit extends Cubit<ChainSwapState> {
 
   Future<OnchainPaymentLimitsResponse> fetchOnchainLimits() async {
     return await _liquidSdk.instance!.fetchOnchainLimits();
+  }
+
+  Future<RecommendedFees> recommendedFees() async {
+    return await _liquidSdk.instance!.recommendedFees();
   }
 
   Future<PreparePayOnchainResponse> preparePayOnchain({
@@ -49,6 +56,56 @@ class ChainSwapCubit extends Cubit<ChainSwapState> {
 
   Future<void> rescanOnchainSwaps() async {
     return await _liquidSdk.instance!.rescanOnchainSwaps();
+  }
+
+  /// Fetches the current recommended fees
+  Future<List<SendChainSwapFeeOption>> fetchSendChainSwapFeeOptions({
+    required int amountSat,
+  }) async {
+    RecommendedFees recommendedFees;
+    try {
+      recommendedFees = await this.recommendedFees();
+      return await _constructFeeOptionList(
+        amountSat: amountSat,
+        recommendedFees: recommendedFees,
+      );
+    } catch (e) {
+      emit(ChainSwapState(error: extractExceptionMessage(e, getSystemAppLocalizations())));
+      rethrow;
+    }
+  }
+
+  Future<List<SendChainSwapFeeOption>> _constructFeeOptionList({
+    required int amountSat,
+    required RecommendedFees recommendedFees,
+  }) async {
+    final recommendedFeeList = [
+      recommendedFees.hourFee,
+      recommendedFees.halfHourFee,
+      recommendedFees.fastestFee,
+    ];
+    final feeOptions = await Future.wait(
+      List.generate(3, (index) async {
+        final recommendedFee = recommendedFeeList.elementAt(index);
+        final preparePayOnchainRequest = PreparePayOnchainRequest(
+          receiverAmountSat: BigInt.from(amountSat),
+          satPerVbyte: recommendedFee.toInt(),
+        );
+        final swapOption = await preparePayOnchain(
+          req: preparePayOnchainRequest,
+        );
+
+        return SendChainSwapFeeOption(
+          txFeeSat: swapOption.claimFeesSat,
+          processingSpeed: ProcessingSpeed.values.elementAt(index),
+          satPerVbyte: recommendedFee,
+          pairInfo: swapOption,
+        );
+      }),
+    );
+
+    emit(state.copyWith(feeOptions: feeOptions));
+    return feeOptions;
   }
 
   void validateSwap(
---
+++ lib/routes/chainswap/send/fee/fee_chooser/widgets/fee_chooser_header.dart
@@ -0,0 +1,60 @@
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:l_breez/cubit/account/account_cubit.dart';
+import 'package:l_breez/routes/chainswap/send/fee/fee_chooser/widgets/fee_option_button.dart';
+import 'package:l_breez/routes/chainswap/send/fee/fee_option.dart';
+
+class FeeChooserHeader extends StatefulWidget {
+  final int amountSat;
+  final List<FeeOption> feeOptions;
+  final int selectedFeeIndex;
+  final Function(int) onSelect;
+
+  const FeeChooserHeader({
+    required this.amountSat,
+    required this.feeOptions,
+    required this.selectedFeeIndex,
+    required this.onSelect,
+    super.key,
+  });
+
+  @override
+  State<FeeChooserHeader> createState() => _FeeChooserHeaderState();
+}
+
+class _FeeChooserHeaderState extends State<FeeChooserHeader> {
+  late List<FeeOptionButton> feeOptionButtons;
+
+  @override
+  Widget build(BuildContext context) {
+    final account = context.read<AccountCubit>().state;
+    final texts = context.texts();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          children: List<FeeOptionButton>.generate(
+            widget.feeOptions.length,
+            (index) {
+              var feeOption = widget.feeOptions.elementAt(index);
+              return FeeOptionButton(
+                index: index,
+                text: feeOption.getDisplayName(texts),
+                isAffordable: feeOption.isAffordable(
+                  balanceSat: account.balance,
+                  walletBalanceSat: account.walletBalance,
+                  amountSat: widget.amountSat,
+                ),
+                isSelected: widget.selectedFeeIndex == index,
+                onSelect: () => widget.onSelect(index),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
