8900d22859eedac9087dfff1fe27b92f6ec08aa9
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-08-19 15:39:05 +0300

    Ignore static analysis warnings for firebase options

--- lib/main/bootstrap.dart
+++ lib/main/bootstrap.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_breez_liquid/flutter_breez_liquid.dart' as liquid_sdk;
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:l_breez/cubit/cubit.dart';
+// ignore: uri_does_not_exist
 import 'package:l_breez/firebase/firebase_options.dart';
 import 'package:l_breez/utils/date.dart';
 import 'package:logging/logging.dart';
@@ -36,6 +37,7 @@ Future<void> bootstrap(AppBuilder builder) async {
     //initializeDateFormatting(Platform.localeName, null);
     BreezDateUtils.setupLocales();
     await Firebase.initializeApp(
+      // ignore: undefined_identifier
       options: DefaultFirebaseOptions.currentPlatform,
     );
     final injector = ServiceInjector();
