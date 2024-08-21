818608792f451c7aab6d8fe71886cd07a6311e03
Ross Savage <hello@satimoto.com>
2024-08-21 13:05:10 +0200

    Fix logging initialisation

--- ios/NotificationService/NotificationService.swift
+++ ios/NotificationService/NotificationService.swift
@@ -8,12 +8,8 @@ class NotificationService: SDKNotificationService {
     fileprivate let TAG = "NotificationService"
     
     private let accountMnemonic: String = "account_mnemonic"
-        
-    override init(logger: Logger) {
-        super.init(logger: logger)
-    }
     
-    convenience init() {
+    override init() {
         let logsDir = FileManager
             .default.containerURL(forSecurityApplicationGroupIdentifier: accessGroup)!.appendingPathComponent("logs")
         let extensionLogFile = logsDir.appendingPathComponent("\(Date().timeIntervalSince1970).ios-extension.log")
@@ -23,17 +19,24 @@ class NotificationService: SDKNotificationService {
             return log
         }()
         
+        super.init()
+
         // Set Notification Service Logger to SdkLogListener that utilizes XCGLogger library
         let sdkLogger = SdkLogListener(logger: xcgLogger)
-                
-        self.init(logger: sdkLogger)
+        self.setLogger(logger: sdkLogger)
+        // Use the same SdkLogListener to listen in on BreezSDKLiquid node logs
+        do {
+            try BreezSDKLiquid.setLogger(logger: sdkLogger)
+        } catch let e {
+            self.logger.log(tag: TAG, line:"Failed to set log stream: \(e)", level: "ERROR")
+        }
     }
     
     override func getConnectRequest() -> ConnectRequest? {
         var config = defaultConfig(network: LiquidNetwork.mainnet)
         config.workingDir = FileManager
             .default.containerURL(forSecurityApplicationGroupIdentifier: accessGroup)!
-            .absoluteString
+            .path
         
         // Construct the ConnectRequest 
         guard let mnemonic = KeychainHelper.shared.getFlutterString(accessGroup: accessGroup, key: accountMnemonic) else {
