5ac2f3ec6a3483a989faa5a5803cc7954f9bcae7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-09-10 14:18:59 +0300

    Do not override ExpiryAndFeeMessage colors on light theme
    closes #174

--- lib/routes/receive_payment/lightning/widgets/expiry_and_fee_message.dart
+++ lib/routes/receive_payment/lightning/widgets/expiry_and_fee_message.dart
@@ -2,7 +2,6 @@ import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:l_breez/cubit/cubit.dart';
-import 'package:l_breez/theme/theme.dart';
 import 'package:l_breez/widgets/warning_box.dart';
 
 class ExpiryAndFeeMessage extends StatelessWidget {
@@ -20,8 +19,6 @@ class ExpiryAndFeeMessage extends StatelessWidget {
         return WarningBox(
           boxPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 0),
           contentPadding: const EdgeInsets.fromLTRB(8, 12, 8, 12),
-          backgroundColor: themeData.isLightTheme ? const Color(0xFFf3f8fc) : null,
-          borderColor: themeData.isLightTheme ? const Color(0xFF0085fb) : null,
           child: Text(
             (feesSat != 0)
                 ? texts
@@ -31,8 +28,8 @@ class ExpiryAndFeeMessage extends StatelessWidget {
                     )
                     .replaceAll(" setup", "")
                 : texts.qr_code_dialog_warning_message,
+            style: themeData.textTheme.titleLarge,
             textAlign: TextAlign.center,
-            style: themeData.primaryTextTheme.bodySmall,
           ),
         );
       },
