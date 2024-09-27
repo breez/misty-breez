be7476de94daa6ff6e80b71c1f7767f04badee63
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-09-27 14:35:43 +0300

    Display SDK initialization errors on BootstrapErrorPage
    Constraint error & stack trace containers and make them scrollable

--- lib/main/bootstrap.dart
+++ lib/main/bootstrap.dart
@@ -11,6 +11,7 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:l_breez/cubit/cubit.dart';
 // ignore: uri_does_not_exist
 import 'package:l_breez/firebase/firebase_options.dart';
+import 'package:l_breez/main/bootstrap_error_page.dart';
 import 'package:l_breez/utils/date.dart';
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as p;
@@ -40,7 +41,7 @@ Future<void> bootstrap(AppBuilder builder) async {
     }
 
     // Initialize library
-    await liquid_sdk.initialize();
+    await _initializeLiquidSdk();
     final injector = ServiceInjector();
     final breezLogger = injector.breezLogger;
     breezLogger.registerBreezSdkLiquidLogs(injector.liquidSDK);
@@ -69,4 +70,13 @@ Future<void> bootstrap(AppBuilder builder) async {
       _log.severe("FlutterError: $error", error, stackTrace);
     }
   });
+}
+
+Future<void> _initializeLiquidSdk() async {
+  try {
+    await liquid_sdk.initialize();
+  } catch (error, stackTrace) {
+    _log.severe("Failed to initialize Liquid SDK: $error", error, stackTrace);
+    runApp(BootstrapErrorPage(error: error, stackTrace: stackTrace));
+  }
 }
