9c8042009631ab3a3c80ad92f5525b4e80e22667
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-10-03 20:19:24 +0300

    Add removal instructions for isDefaultTitleWithLiquidNaming

--- lib/utils/extensions/payment_title_extension.dart
+++ lib/utils/extensions/payment_title_extension.dart
@@ -1,4 +1,5 @@
 extension PaymentTitleExtension on String {
+  // TODO: Remove once default descriptions from the SDK is removed
   bool get isDefaultTitleWithLiquidNaming {
     return contains("Send to L-BTC address") || contains("Liquid transfer");
   }
