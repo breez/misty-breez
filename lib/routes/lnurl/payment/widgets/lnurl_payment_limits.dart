68002fd533b2fdcb20d20f9a677e769ecae0d641
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-10-22 14:53:57 +0300

    Display error message if fetching lightning payment limits fails

--- lib/routes/lnurl/payment/widgets/lnurl_payment_limits.dart
+++ lib/routes/lnurl/payment/widgets/lnurl_payment_limits.dart
@@ -1,19 +1,25 @@
+import 'dart:math';
+
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
 import 'package:l_breez/cubit/cubit.dart';
 import 'package:l_breez/theme/src/theme.dart';
 
 class LnUrlPaymentLimits extends StatelessWidget {
-  final int effectiveMinSat;
-  final int effectiveMaxSat;
+  final LightningPaymentLimitsResponse? limitsResponse;
+  final int minSendableSat;
+  final int maxSendableSat;
   final Future<void> Function(dynamic amount) onTap;
 
   const LnUrlPaymentLimits({
     super.key,
-    required this.effectiveMinSat,
-    required this.effectiveMaxSat,
+    required this.limitsResponse,
+    required this.minSendableSat,
+    required this.maxSendableSat,
     required this.onTap,
   });
 
@@ -23,7 +29,26 @@ class LnUrlPaymentLimits extends StatelessWidget {
     final currencyState = currencyCubit.state;
 
     final texts = context.texts();
+    final themeData = Theme.of(context);
 
+    if (limitsResponse == null) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: AutoSizeText(
+          "Failed to fetch payment limits.",
+          maxLines: 3,
+          textAlign: TextAlign.left,
+          style: FieldTextStyle.labelStyle.copyWith(
+            color: themeData.isLightTheme ? Colors.red : themeData.colorScheme.error,
+          ),
+        ),
+      );
+    }
+    final effectiveMinSat = min(
+      max(limitsResponse!.send.minSat.toInt(), minSendableSat),
+      limitsResponse!.send.maxSat.toInt(),
+    );
+    final effectiveMaxSat = min(limitsResponse!.send.maxSat.toInt(), maxSendableSat);
     final effMinSendableFormatted = currencyState.bitcoinCurrency.format(effectiveMinSat);
     final effMaxSendableFormatted = currencyState.bitcoinCurrency.format(effectiveMaxSat);
 
