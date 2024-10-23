376dc77317897970eb8796bb3d91ba9050759f71
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-10-23 13:32:33 +0300

    Convert SuccessAction dialog into fullscreen dialog
    Display scrollbar by default on the message
    Use the same message widget on ShareablePaymentRow

--- lib/routes/lnurl/payment/success_action/success_action_dialog.dart
+++ lib/routes/lnurl/payment/success_action/success_action_dialog.dart
@@ -1,7 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:l_breez/theme/theme.dart';
 import 'package:l_breez/widgets/shareable_payment_row.dart';
+import 'package:l_breez/widgets/single_button_bottom_bar.dart';
 
 class SuccessActionDialog extends StatefulWidget {
   final String message;
@@ -19,56 +22,59 @@ class SuccessActionDialogState extends State<SuccessActionDialog> {
     final themeData = Theme.of(context);
     final texts = context.texts();
 
-    return AlertDialog(
-      title: Text(texts.ln_url_success_action_title),
-      content: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            if (widget.url == null) Message(widget.message),
-            if (widget.url != null) ...[
-              ShareablePaymentRow(
-                title: widget.message,
-                sharedValue: widget.url!,
-                isURL: true,
-                isExpanded: true,
-                titleTextStyle: themeData.primaryTextTheme.displaySmall!.copyWith(fontSize: 16),
-                childrenTextStyle: themeData.primaryTextTheme.displaySmall!.copyWith(
-                  fontSize: 12,
-                  height: 1.5,
-                  color: Colors.blue,
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: Theme.of(context).appBarTheme.systemOverlayStyle!.copyWith(
+            systemNavigationBarColor: Theme.of(context).colorScheme.surface,
+          ),
+      child: Dialog.fullscreen(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Expanded(child: SizedBox.expand()),
+              Text(
+                texts.ln_url_success_action_title,
+                style: themeData.dialogTheme.titleTextStyle,
+              ),
+              Column(
+                children: [
+                  widget.url == null
+                      ? Message(widget.message)
+                      : ShareablePaymentRow(
+                          title: widget.message,
+                          titleWidget: Message(widget.message),
+                          sharedValue: widget.url!,
+                          isURL: true,
+                          isExpanded: true,
+                          titleTextStyle: themeData.primaryTextTheme.displaySmall!.copyWith(fontSize: 16),
+                          childrenTextStyle: themeData.primaryTextTheme.displaySmall!.copyWith(
+                            fontSize: 12,
+                            height: 1.5,
+                            color: Colors.blue,
+                          ),
+                          iconPadding: EdgeInsets.zero,
+                          tilePadding: EdgeInsets.zero,
+                          childrenPadding: EdgeInsets.zero,
+                        ),
+                ],
+              ),
+              const Expanded(child: SizedBox.expand()),
+              Theme(
+                data: breezDarkTheme,
+                child: SingleButtonBottomBar(
+                  text: texts.lnurl_withdraw_dialog_action_close,
+                  stickToBottom: false,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
                 ),
-                iconPadding: EdgeInsets.zero,
-                tilePadding: EdgeInsets.zero,
-                childrenPadding: EdgeInsets.zero,
               ),
             ],
-          ],
+          ),
         ),
       ),
-      contentPadding: const EdgeInsets.only(top: 16.0, left: 32.0, right: 32.0),
-      actions: [
-        TextButton(
-          style: ButtonStyle(
-            overlayColor: WidgetStateProperty.resolveWith<Color>((states) {
-              if (states.contains(WidgetState.pressed)) {
-                return Colors.transparent;
-              }
-              // Defer to the widget's default.
-              return themeData.textTheme.labelLarge!.color!;
-            }),
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text(
-            texts.lnurl_withdraw_dialog_action_close,
-            style: themeData.primaryTextTheme.labelLarge,
-          ),
-        ),
-      ],
     );
   }
 }
@@ -82,13 +88,15 @@ class Message extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeData = Theme.of(context);
     return Padding(
-      padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      padding: const EdgeInsets.only(top: 24.0, bottom: 8.0),
       child: Container(
         constraints: const BoxConstraints(
           maxHeight: 200,
           minWidth: double.infinity,
         ),
         child: Scrollbar(
+          radius: const Radius.circular(16.0),
+          thumbVisibility: true,
           child: SingleChildScrollView(
             child: AutoSizeText(
               message,
