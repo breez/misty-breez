73d5443b25b815e97c8453c3df9e32ecef474831
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-06 15:38:21 +0300

    Map PaymentError's to user friendly error messages

--- lib/utils/exceptions.dart
+++ lib/utils/exceptions.dart
@@ -25,10 +25,54 @@ String extractExceptionMessage(
     message = _localizedExceptionMessage(texts, message);
     return message;
   }
-  if (exception is PaymentError_InsufficientFunds) {
-    return texts.invoice_payment_validator_error_insufficient_local_balance;
+  if (exception is PaymentError) {
+    return _getPaymentErrorMessage(exception, texts);
   }
   return _extractInnerErrorMessage(exception.toString()) ?? defaultErrorMsg ?? exception.toString();
+}
+
+String _getPaymentErrorMessage(PaymentError error, BreezTranslations texts) {
+  String message = error.toString();
+  if (error is PaymentError_AlreadyClaimed) {
+    message = "The specified funds have already been claimed";
+  } else if (error is PaymentError_AlreadyPaid) {
+    message = "The specified funds have already been sent";
+  } else if (error is PaymentError_PaymentInProgress) {
+    message = "The payment is already in progress";
+  } else if (error is PaymentError_AmountOutOfRange) {
+    message = "Amount is out of range";
+  } else if (error is PaymentError_AmountMissing) {
+    message = "Amount is missing ${error.err}";
+  } else if (error is PaymentError_InvalidNetwork) {
+    message = "Invalid network ${error.err}";
+  } else if (error is PaymentError_Generic) {
+    message = "Generic error: ${error.err}";
+  } else if (error is PaymentError_InvalidOrExpiredFees) {
+    message = "The provided fees have expired";
+  } else if (error is PaymentError_InsufficientFunds) {
+    message = texts.invoice_payment_validator_error_insufficient_local_balance;
+  } else if (error is PaymentError_InvalidDescription) {
+    message = "Invalid description ${error.err}";
+  } else if (error is PaymentError_InvalidInvoice) {
+    message = "The specified invoice is not valid: ${error.err}";
+  } else if (error is PaymentError_InvalidPreimage) {
+    message = "The generated preimage is not valid";
+  } else if (error is PaymentError_LwkError) {
+    message = "Lwk error: ${error.err}";
+  } else if (error is PaymentError_PairsNotFound) {
+    message = "Boltz did not return any pairs from the request";
+  } else if (error is PaymentError_PaymentTimeout) {
+    message = "The payment timed out";
+  } else if (error is PaymentError_PersistError) {
+    message = "Could not store the swap details locally";
+  } else if (error is PaymentError_SelfTransferNotSupported) {
+    message = "The payment is a self-transfer, which is not supported";
+  } else if (error is PaymentError_SendError) {
+    message = error.err;
+  } else if (error is PaymentError_SignerError) {
+    message = "Could not sign the transaction: ${error.err}";
+  }
+  return message;
 }
 
 String? _extractInnerErrorMessage(String content) {
