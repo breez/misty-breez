e5d2beee3c1a4b530f38d18ada93a47da6ebda81
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-20 18:18:09 +0300

    chore: export LnOfferPaymentPage route

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
 import 'package:l_breez/cubit/cubit.dart';
 import 'package:l_breez/routes/routes.dart';
-import 'package:l_breez/routes/send_payment/lightning/ln_offer_payment_page.dart';
 import 'package:l_breez/widgets/widgets.dart';
 import 'package:logging/logging.dart';
 import 'package:service_injector/service_injector.dart';
