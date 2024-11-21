4b39fc617a10e0347bc2e31d367b6761b09b7bf7
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-21 17:28:10 +0300

    Add a placeholder image on LNURL payments

--- lib/routes/lnurl/widgets/lnurl_metadata_text.dart
+++ lib/routes/lnurl/widgets/lnurl_metadata_text.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:typed_data';
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:l_breez/theme/theme.dart';
@@ -39,35 +36,5 @@ class _LNURLMetadataTextState extends State<LNURLMetadataText> {
         ),
       ),
     );
-  }
-}
-
-class LNURLMetadataImage extends StatelessWidget {
-  final String? base64String;
-
-  const LNURLMetadataImage({super.key, this.base64String});
-
-  @override
-  Widget build(BuildContext context) {
-    if (base64String != null) {
-      final Uint8List bytes = base64Decode(base64String!);
-      if (bytes.isNotEmpty) {
-        const double imageSize = 128.0;
-        return ConstrainedBox(
-          constraints: const BoxConstraints(
-            minHeight: imageSize,
-            minWidth: imageSize,
-            maxWidth: imageSize,
-            maxHeight: imageSize,
-          ),
-          child: Image.memory(
-            bytes,
-            width: imageSize,
-            fit: BoxFit.fitWidth,
-          ),
-        );
-      }
-    }
-    return const SizedBox.shrink();
   }
 }
