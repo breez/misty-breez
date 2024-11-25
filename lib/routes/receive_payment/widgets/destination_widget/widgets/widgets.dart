ce796ceba24d25c103228ff8f3694e23eb203a76
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-25 16:54:46 +0300

    Remove particle animations
    * Remove simple_animations plugin

--- lib/routes/receive_payment/widgets/destination_widget/widgets/widgets.dart
+++ lib/routes/receive_payment/widgets/destination_widget/widgets/widgets.dart
@@ -4,4 +4,3 @@ export 'destination_qr_image.dart';
 export 'destination_qr_widget.dart';
 export 'destination_widget_placeholder.dart';
 export 'loading_or_error.dart';
-export 'particle_animations/particles_animations.dart';
