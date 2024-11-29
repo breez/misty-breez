2f1aed8d897d0a1ab563e0fba46571ceb11a0161
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-29 21:04:06 +0300

    Add bottom padding to LnPayment description

--- lib/routes/send_payment/lnurl/widgets/lnurl_payment_description.dart
+++ lib/routes/send_payment/lnurl/widgets/lnurl_payment_description.dart
@@ -15,28 +15,31 @@ class LnPaymentDescription extends StatelessWidget {
     final BreezTranslations texts = context.texts();
     final ThemeData themeData = Theme.of(context);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        AutoSizeText(
-          texts.ln_payment_description_label,
-          style: themeData.primaryTextTheme.headlineMedium?.copyWith(
-            fontSize: 18.0,
-            color: Colors.white,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          AutoSizeText(
+            texts.ln_payment_description_label,
+            style: themeData.primaryTextTheme.headlineMedium?.copyWith(
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.left,
+            maxLines: 1,
           ),
-          textAlign: TextAlign.left,
-          maxLines: 1,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: WarningBox(
-            boxPadding: EdgeInsets.zero,
-            backgroundColor: themeData.primaryColorLight.withOpacity(0.1),
-            borderColor: themeData.primaryColorLight.withOpacity(0.7),
-            child: LNURLMetadataText(metadataText: metadataText),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: WarningBox(
+              boxPadding: EdgeInsets.zero,
+              backgroundColor: themeData.primaryColorLight.withOpacity(0.1),
+              borderColor: themeData.primaryColorLight.withOpacity(0.7),
+              child: LNURLMetadataText(metadataText: metadataText),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
