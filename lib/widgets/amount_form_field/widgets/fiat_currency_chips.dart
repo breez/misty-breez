bad98b4eabd321dd9742abf07bc4cc0fd784f7df
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2024-11-29 13:05:25 +0300

    Make fiat currency chips scrollable

--- lib/widgets/amount_form_field/widgets/fiat_currency_chips.dart
+++ lib/widgets/amount_form_field/widgets/fiat_currency_chips.dart
@@ -23,32 +23,38 @@ class FiatCurrencyChips extends StatelessWidget {
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Wrap(
-        spacing: 8.0,
-        children: currencyState.preferredCurrencies.map(
-          (String currencyId) {
-            final FiatCurrency fiatCurrency =
-                currencyState.fiatCurrenciesData.firstWhere((FiatCurrency c) => c.id == currencyId);
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: currencyState.preferredCurrencies.map(
+            (String currencyId) {
+              final FiatCurrency fiatCurrency = currencyState.fiatCurrenciesData.firstWhere(
+                (FiatCurrency c) => c.id == currencyId,
+              );
 
-            return ChoiceChip(
-              label: Text(fiatCurrency.id),
-              selected: selectedCurrency == fiatCurrency.id,
-              onSelected: (bool selected) {
-                if (selected) {
-                  onCurrencySelected(fiatCurrency.id);
-                }
-              },
-              selectedColor: themeData.chipTheme.backgroundColor,
-              backgroundColor:
-                  themeData.isLightTheme ? themeData.primaryColorDark : Colors.white.withAlpha(0x1f),
-              labelStyle: TextStyle(
-                color: selectedCurrency == fiatCurrency.id
-                    ? Colors.white
-                    : themeData.textTheme.bodyMedium?.color,
-              ),
-            );
-          },
-        ).toList(),
+              return Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: ChoiceChip(
+                  label: Text(fiatCurrency.id),
+                  selected: selectedCurrency == fiatCurrency.id,
+                  onSelected: (bool selected) {
+                    if (selected) {
+                      onCurrencySelected(fiatCurrency.id);
+                    }
+                  },
+                  selectedColor: themeData.chipTheme.backgroundColor,
+                  backgroundColor:
+                      themeData.isLightTheme ? themeData.primaryColorDark : Colors.white.withAlpha(0x1f),
+                  labelStyle: TextStyle(
+                    color: selectedCurrency == fiatCurrency.id
+                        ? Colors.white
+                        : themeData.textTheme.bodyMedium?.color,
+                  ),
+                ),
+              );
+            },
+          ).toList(),
+        ),
       ),
     );
   }
