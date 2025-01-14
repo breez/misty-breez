f4f04b9658dc975562386689de956f5091d87909
Daniel Granhão <daniel.granhao@gmail.com>
2025-01-14 12:32:43 +0000

    Use available translations

--- lib/routes/home/widgets/payments_list/widgets/payment_details_sheet/widgets/payment_details_sheet_lnurl_ln_address.dart
+++ lib/routes/home/widgets/payments_list/widgets/payment_details_sheet/widgets/payment_details_sheet_lnurl_ln_address.dart
@@ -1,5 +1,5 @@
-//import 'package:breez_translations/breez_translations_locales.dart';
-//import 'package:breez_translations/generated/breez_translations.dart';
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:breez_translations/generated/breez_translations.dart';
 import 'package:flutter/material.dart';
 import 'package:l_breez/widgets/widgets.dart';
 
@@ -10,14 +10,13 @@ class PaymentDetailsSheetLnUrlLnAddress extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //final BreezTranslations texts = context.texts();
+    final BreezTranslations texts = context.texts();
     final ThemeData themeData = Theme.of(context);
 
     return ShareablePaymentRow(
       tilePadding: EdgeInsets.zero,
       dividerColor: Colors.transparent,
-      // TODO(danielgranhao): translate.
-      title: 'Lightning Address:',
+      title: '${texts.payment_details_dialog_share_lightning_address}:',
       titleTextStyle: themeData.primaryTextTheme.headlineMedium?.copyWith(
         fontSize: 18.0,
         color: Colors.white,
