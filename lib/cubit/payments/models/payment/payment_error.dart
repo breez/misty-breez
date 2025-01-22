3d715724f024c13b9fac02307398766c9c18ea18
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-01-22 15:38:17 +0300

    Add toString methods to PaymentValidator exceptions.
    - Refactor PaymentValidator
      - Change log messages
      - Handles exceptions via a helper method, handleException

--- lib/cubit/payments/models/payment/payment_error.dart
+++ lib/cubit/payments/models/payment/payment_error.dart
@@ -2,40 +2,65 @@ class PaymentExceededLimitError implements Exception {
   final int limitSat;
 
   const PaymentExceededLimitError(this.limitSat);
+
+  @override
+  String toString() => 'Payment amount exceeds the limit of $limitSat satoshis.';
 }
 
 class PaymentBelowLimitError implements Exception {
   final int limitSat;
 
   const PaymentBelowLimitError(this.limitSat);
+
+  @override
+  String toString() => 'Payment amount is below the minimum limit of $limitSat satoshis.';
 }
 
 class PaymentBelowReserveError implements Exception {
   final int reserveAmount;
 
   const PaymentBelowReserveError(this.reserveAmount);
+
+  @override
+  String toString() => 'Payment amount is below the reserve amount of $reserveAmount satoshis.';
 }
 
 class InsufficientLocalBalanceError implements Exception {
   const InsufficientLocalBalanceError();
+
+  @override
+  String toString() => 'Insufficient local balance to process the payment.';
 }
 
 class PaymentBelowSetupFeesError implements Exception {
   final int setupFees;
 
   const PaymentBelowSetupFeesError(this.setupFees);
+
+  @override
+  String toString() => 'Payment amount is below the setup fees of $setupFees satoshis.';
 }
 
-class PaymentExceedededLiquidityError implements Exception {
+class PaymentExceededLiquidityError implements Exception {
   final int limitSat;
 
-  const PaymentExceedededLiquidityError(this.limitSat);
+  const PaymentExceededLiquidityError(this.limitSat);
+
+  @override
+  String toString() => 'Payment amount exceeds the available liquidity of $limitSat satoshis.';
 }
 
 class PaymentExceededLiquidityChannelCreationNotPossibleError implements Exception {
   final int limitSat;
 
   const PaymentExceededLiquidityChannelCreationNotPossibleError(this.limitSat);
+
+  @override
+  String toString() =>
+      'Payment amount exceeds available liquidity of $limitSat satoshis, and channel creation is not possible.';
 }
 
-class NoChannelCreationZeroLiquidityError implements Exception {}
+class NoChannelCreationZeroLiquidityError implements Exception {
+  @override
+  String toString() => 'Channel creation is not possible due to zero liquidity.';
+}
