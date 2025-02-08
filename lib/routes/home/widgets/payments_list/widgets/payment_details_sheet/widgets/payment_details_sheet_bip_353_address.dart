124409825af4e0ec940dca9c1d624d8d102f9add
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-02-08 07:55:15 +0300

    Use "Lightning Address:" as title for BIP 353 Addresses on payment details

--- lib/routes/home/widgets/payments_list/widgets/payment_details_sheet/widgets/payment_details_sheet_bip_353_address.dart
+++ lib/routes/home/widgets/payments_list/widgets/payment_details_sheet/widgets/payment_details_sheet_bip_353_address.dart
@@ -1,3 +1,5 @@
+import 'package:breez_translations/breez_translations_locales.dart';
+import 'package:breez_translations/generated/breez_translations.dart';
 import 'package:flutter/material.dart';
 import 'package:l_breez/widgets/widgets.dart';
 
@@ -8,12 +10,13 @@ class PaymentDetailsSheetBip353Address extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final BreezTranslations texts = context.texts();
     final ThemeData themeData = Theme.of(context);
 
     return ShareablePaymentRow(
       tilePadding: EdgeInsets.zero,
       dividerColor: Colors.transparent,
-      title: 'BIP 353 Address:',
+      title: '${texts.payment_details_dialog_share_lightning_address}:',
       titleTextStyle: themeData.primaryTextTheme.headlineMedium?.copyWith(
         fontSize: 18.0,
         color: Colors.white,
