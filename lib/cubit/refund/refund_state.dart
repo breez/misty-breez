7dde9245e3906b333416953421b312aaf9aed25a
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-02-11 15:05:34 +0300

    refactor: Add extension to identify refund related SDK events

--- lib/cubit/refund/refund_state.dart
+++ lib/cubit/refund/refund_state.dart
@@ -27,3 +27,20 @@ class RefundState {
 
   bool get hasRefundables => refundables?.isNotEmpty ?? false;
 }
+
+extension RefundRelatedSdkEvent on SdkEvent {
+  /// Returns true if the event is related to a refund.
+  ///
+  /// For [SdkEvent_PaymentFailed], the [hasRefundables] flag must be true.
+  bool isRefundRelated({bool hasRefundables = false}) {
+    if (this is SdkEvent_PaymentRefundable ||
+        this is SdkEvent_PaymentRefundPending ||
+        this is SdkEvent_PaymentRefunded) {
+      return true;
+    }
+    if (this is SdkEvent_PaymentFailed && hasRefundables) {
+      return true;
+    }
+    return false;
+  }
+}
