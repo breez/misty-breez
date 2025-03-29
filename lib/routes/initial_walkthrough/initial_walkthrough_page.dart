45c2050cb63be022f035c39162a725acba0bf40b
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-03-30 00:35:06 +0300

    Replace 'Restore from backup' link with a 'Restore' button
    Fixes #472
    
    Simplified layout for maintainability.
    
    Added semantics to Let's Breez & Restore buttons for a11y.

--- lib/routes/initial_walkthrough/initial_walkthrough_page.dart
+++ lib/routes/initial_walkthrough/initial_walkthrough_page.dart
@@ -1,15 +1,17 @@
+import 'dart:math';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:breez_translations/generated/breez_translations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logging/logging.dart';
 import 'package:misty_breez/cubit/cubit.dart';
 import 'package:misty_breez/routes/routes.dart';
 import 'package:misty_breez/theme/theme.dart';
 import 'package:misty_breez/utils/utils.dart';
 import 'package:misty_breez/widgets/widgets.dart';
-import 'package:logging/logging.dart';
 import 'package:theme_provider/theme_provider.dart';
 
 final Logger _logger = Logger('InitialWalkthrough');
@@ -62,6 +64,7 @@ class InitialWalkthroughPageState extends State<InitialWalkthroughPage>
   Widget build(BuildContext context) {
     final BreezTranslations texts = context.texts();
     final ThemeData themeData = Theme.of(context);
+    final Size screenSize = MediaQuery.of(context).size;
 
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: themeData.appBarTheme.systemOverlayStyle!.copyWith(
@@ -71,85 +74,88 @@ class InitialWalkthroughPageState extends State<InitialWalkthroughPage>
         data: breezLightTheme,
         child: Scaffold(
           key: _scaffoldKey,
-          body: Padding(
-            padding: const EdgeInsets.only(top: 24.0),
-            child: Stack(
+          body: SafeArea(
+            child: Column(
               children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Expanded(
-                      flex: 200,
-                      child: Container(),
-                    ),
-                    Expanded(
-                      flex: 171,
-                      child: AnimatedBuilder(
-                        animation: _animation!,
-                        builder: (BuildContext context, Widget? child) {
-                          final String frame = _animation!.value.toString().padLeft(2, '0');
-                          return Image.asset(
-                            'assets/animations/welcome/frame_${frame}_delay-0.04s.png',
-                            gaplessPlayback: true,
-                            fit: BoxFit.cover,
-                          );
-                        },
+                const Spacer(flex: 11),
+
+                // Logo animation
+                AnimatedBuilder(
+                  animation: _animation!,
+                  builder: (BuildContext context, Widget? child) {
+                    final String frame = _animation!.value.toString().padLeft(2, '0');
+                    return SizedBox(
+                      height: screenSize.height * 0.19,
+                      child: Image.asset(
+                        'assets/animations/welcome/frame_${frame}_delay-0.04s.png',
+                        gaplessPlayback: true,
+                        fit: BoxFit.cover,
                       ),
-                    ),
-                    Expanded(
-                      flex: 200,
-                      child: Container(),
-                    ),
-                    Expanded(
-                      flex: 48,
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 24, right: 24),
-                        child: AutoSizeText(
-                          texts.initial_walk_through_welcome_message,
-                          textAlign: TextAlign.center,
-                          style: welcomeTextStyle,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 60,
-                      child: Container(),
-                    ),
-                    SizedBox(
-                      height: 48.0,
-                      width: 168.0,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
-                          backgroundColor: themeData.primaryColor,
-                          elevation: 0.0,
-                          shape: const StadiumBorder(),
-                        ),
-                        child: Text(
-                          texts.initial_walk_through_lets_breeze,
-                          style: themeData.textTheme.labelLarge,
-                        ),
-                        onPressed: () => _letsBreez(),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 40,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: GestureDetector(
-                          onTap: () => _restoreWalletFromMnemonicSeed(),
-                          child: Text(
-                            texts.initial_walk_through_restore_from_backup,
-                            style: restoreLinkStyle,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      flex: 120,
-                      child: Container(),
-                    ),
-                  ],
+                    );
+                  },
                 ),
+
+                const Spacer(flex: 13),
+
+                // Welcome text
+                AutoSizeText(
+                  texts.initial_walk_through_welcome_message,
+                  textAlign: TextAlign.center,
+                  style: welcomeTextStyle,
+                ),
+
+                const Spacer(flex: 5),
+
+                // Let's Breez button
+                SizedBox(
+                  height: 48.0,
+                  width: min(screenSize.width * 0.4, 168),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                      backgroundColor: themeData.primaryColor,
+                      elevation: 0.0,
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () => _letsBreez(),
+                    child: Semantics(
+                      button: true,
+                      label: 'Start using Breez',
+                      child: Text(
+                        texts.initial_walk_through_lets_breeze,
+                        style: themeData.textTheme.labelLarge,
+                      ),
+                    ),
+                  ),
+                ),
+
+                const Spacer(),
+
+                // Restore Button
+                SizedBox(
+                  height: 48.0,
+                  width: min(screenSize.width * 0.4, 168),
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Colors.white),
+                      padding: const EdgeInsets.fromLTRB(16, 4, 16, 4),
+                      elevation: 0.0,
+                      shape: const StadiumBorder(),
+                    ),
+                    onPressed: () => _restoreWalletFromMnemonicSeed(),
+                    child: Semantics(
+                      button: true,
+                      label: 'Restore using mnemonics',
+                      child: const Text(
+                        'RESTORE',
+                        style: balanceFiatConversionTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Bottom Spacing
+                const Spacer(flex: 4),
               ],
             ),
           ),
@@ -159,7 +165,7 @@ class InitialWalkthroughPageState extends State<InitialWalkthroughPage>
   }
 
   void _letsBreez() async {
-    _logger.info('Lets breez');
+    _logger.info("Let's Breez!");
     final bool approved = await showDialog(
       useRootNavigator: false,
       context: context,
