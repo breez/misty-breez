8868562f7982ee8731c70eba3847ad003eafb145
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-04-02 13:46:47 +0300

    Open Receive Payment page upon clicking Receive on bottom action bar

--- lib/routes/home/widgets/bottom_actions_bar/bottom_actions_bar.dart
+++ lib/routes/home/widgets/bottom_actions_bar/bottom_actions_bar.dart
@@ -2,11 +2,11 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:breez_translations/generated/breez_translations.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:misty_breez/routes/routes.dart';
-import 'package:misty_breez/theme/theme.dart';
 
 export 'widgets/widgets.dart';
+
+final AutoSizeGroup actionsGroup = AutoSizeGroup();
 
 class BottomActionsBar extends StatelessWidget {
   final GlobalKey firstPaymentItemKey;
@@ -18,7 +18,7 @@ class BottomActionsBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final AutoSizeGroup actionsGroup = AutoSizeGroup();
+    final BreezTranslations texts = context.texts();
 
     return BottomAppBar(
       child: SizedBox(
@@ -26,84 +26,31 @@ class BottomActionsBar extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            SendOptions(
-              firstPaymentItemKey: firstPaymentItemKey,
-              actionsGroup: actionsGroup,
+            BottomActionItem(
+              onPress: () {
+                Navigator.of(context).pushNamed(
+                  EnterPaymentInfoPage.routeName,
+                );
+              },
+              group: actionsGroup,
+              text: texts.bottom_action_bar_send,
+              iconAssetPath: 'assets/icons/send-action.png',
             ),
             Container(width: 64),
-            ReceiveOptions(
-              firstPaymentItemKey: firstPaymentItemKey,
-              actionsGroup: actionsGroup,
+            BottomActionItem(
+              onPress: () {
+                Navigator.of(context).pushNamed(
+                  ReceivePaymentPage.routeName,
+                  arguments: ReceiveLightningAddressPage.pageIndex,
+                );
+              },
+              group: actionsGroup,
+              text: texts.bottom_action_bar_receive,
+              iconAssetPath: 'assets/icons/receive-action.png',
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class SendOptions extends StatelessWidget {
-  final GlobalKey<State<StatefulWidget>> firstPaymentItemKey;
-  final AutoSizeGroup actionsGroup;
-
-  const SendOptions({
-    required this.firstPaymentItemKey,
-    required this.actionsGroup,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final BreezTranslations texts = context.texts();
-
-    return BottomActionItem(
-      onPress: () {
-        Navigator.of(context).pushNamed(
-          EnterPaymentInfoPage.routeName,
-        );
-      },
-      group: actionsGroup,
-      text: texts.bottom_action_bar_send,
-      iconAssetPath: 'assets/icons/send-action.png',
-    );
-  }
-}
-
-class ReceiveOptions extends StatelessWidget {
-  final GlobalKey<State<StatefulWidget>> firstPaymentItemKey;
-  final AutoSizeGroup actionsGroup;
-
-  const ReceiveOptions({
-    required this.firstPaymentItemKey,
-    required this.actionsGroup,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData themeData = Theme.of(context);
-    final BreezTranslations texts = context.texts();
-
-    return BottomActionItem(
-      onPress: () => showModalBottomSheet(
-        context: context,
-        backgroundColor: themeData.customData.paymentListBgColor,
-        builder: (BuildContext context) {
-          return AnnotatedRegion<SystemUiOverlayStyle>(
-            value: Theme.of(context).appBarTheme.systemOverlayStyle!.copyWith(
-                  systemNavigationBarColor: Theme.of(context).canvasColor,
-                ),
-            child: SafeArea(
-              child: ReceiveOptionsBottomSheet(
-                firstPaymentItemKey: firstPaymentItemKey,
-              ),
-            ),
-          );
-        },
-      ),
-      group: actionsGroup,
-      text: texts.bottom_action_bar_receive,
-      iconAssetPath: 'assets/icons/receive-action.png',
     );
   }
 }
--- lib/routes/home/widgets/bottom_actions_bar/widgets/widgets.dart
+++ lib/routes/home/widgets/bottom_actions_bar/widgets/widgets.dart
@@ -1,3 +1,1 @@
 export 'bottom_action_item.dart';
-export 'bottom_action_item_image.dart';
-export 'receive_options_bottom_sheet.dart';
