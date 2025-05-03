87e00424ef3e856fbec90e8541781d0509549d2b
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-05-03 15:27:14 +0300

    add fee information to the Lightning Address screen (#518)
    Fixes #512

--- lib/routes/receive_payment/widgets/payment_message_boxes/payment_info_message_box.dart
+++ lib/routes/receive_payment/widgets/payment_message_boxes/payment_info_message_box.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:misty_breez/services/services.dart';
 import 'package:misty_breez/widgets/widgets.dart';
 
 class PaymentInfoMessageBox extends StatelessWidget {
   final String message;
+  final String? linkUrl;
 
-  const PaymentInfoMessageBox({required this.message, super.key});
+  const PaymentInfoMessageBox({required this.message, this.linkUrl, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +17,40 @@ class PaymentInfoMessageBox extends StatelessWidget {
       boxPadding: EdgeInsets.zero,
       backgroundColor: themeData.colorScheme.error.withValues(alpha: .1),
       contentPadding: const EdgeInsets.all(16.0),
-      child: Text(
-        message,
-        style: themeData.textTheme.titleLarge?.copyWith(
-          color: themeData.colorScheme.error,
-        ),
-        textAlign: TextAlign.center,
-      ),
+      child: linkUrl != null
+          ? RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                text: message,
+                style: themeData.textTheme.titleLarge?.copyWith(
+                  color: themeData.colorScheme.error,
+                ),
+                children: <InlineSpan>[
+                  TextSpan(
+                    text: 'here',
+                    style: themeData.textTheme.titleLarge?.copyWith(
+                      color: themeData.colorScheme.error,
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () => ExternalBrowserService.launchLink(context, linkAddress: linkUrl!),
+                  ),
+                  TextSpan(
+                    text: '.',
+                    style: themeData.textTheme.titleLarge?.copyWith(
+                      color: themeData.colorScheme.error,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Text(
+              message,
+              style: themeData.textTheme.titleLarge?.copyWith(
+                color: themeData.colorScheme.error,
+              ),
+              textAlign: TextAlign.center,
+            ),
     );
   }
 }
