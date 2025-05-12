adeb08634fbc04454b8de0f655eb9bc51a6f53cc
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-05-12 16:42:13 +0300

    refactor: remove PaymentStreamFactory now that we rely on a single stream

--- lib/services/payment_tracking/payment_tracking.dart
+++ lib/services/payment_tracking/payment_tracking.dart
@@ -1,3 +1,2 @@
-export 'payment_stream_factory.dart';
 export 'payment_tracking_config.dart';
 export 'payment_tracking_service.dart';
--- lib/services/payment_tracking/payment_tracking_service.dart
+++ lib/services/payment_tracking/payment_tracking_service.dart
@@ -1,34 +1,42 @@
 import 'dart:async';
 
 import 'package:breez_sdk_liquid/breez_sdk_liquid.dart';
+import 'package:flutter_breez_liquid/flutter_breez_liquid.dart';
+import 'package:logging/logging.dart';
+import 'package:misty_breez/models/models.dart';
 import 'package:misty_breez/services/services.dart';
+
+final Logger _logger = Logger('PaymentTrackingService');
 
 /// Service responsible for tracking payments of various types
 class PaymentTrackingService {
   final BreezSDKLiquid _breezSdkLiquid;
 
-  // Helper components
-  late final PaymentStreamFactory _streamFactory;
-  StreamSubscription<dynamic>? _activeSubscription;
+  StreamSubscription<Payment>? _activeSubscription;
 
-  PaymentTrackingService(this._breezSdkLiquid) {
-    _streamFactory = PaymentStreamFactory(_breezSdkLiquid);
-  }
+  PaymentTrackingService(this._breezSdkLiquid);
 
   /// Start tracking payments based on the provided configuration
   Future<void> startTracking({required PaymentTrackingConfig config}) async {
-    _resetTrackingState();
-    _activeSubscription = await _streamFactory.subscribeToStream(config);
+    dispose();
+    if (config is ReceivePaymentTrackingConfig && config.trackingDelay != Duration.zero) {
+      await Future<void>.delayed(config.trackingDelay);
+    }
+
+    _activeSubscription = _breezSdkLiquid.paymentEventStream
+        .where((PaymentEvent event) => event.payment.matches(config))
+        .map((PaymentEvent e) => e.payment)
+        .listen(
+      (Payment payment) {
+        config.onPaymentComplete(true);
+      },
+      onError: (Object error) {
+        _logger.warning('Failed to track payment.', error);
+        config.onPaymentComplete(false);
+      },
+    );
   }
 
   /// Clean up resources and stop tracking
-  void dispose() {
-    _resetTrackingState();
-    _streamFactory.dispose();
-  }
-
-  void _resetTrackingState() {
-    _activeSubscription?.cancel();
-    _activeSubscription = null;
-  }
+  void dispose() => _activeSubscription?.cancel();
 }
