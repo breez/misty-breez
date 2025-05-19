2c55ebe4251b68a000ef9f73c7f6ce7177003282
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-05-19 15:00:59 +0300

    ui: show user avatar information for incoming Bolt 12 Offer payments on the payment list & details
    Fixes #545

--- lib/models/extensions/payment_title_extension.dart
+++ lib/models/extensions/payment_title_extension.dart
@@ -1,6 +1,9 @@
 extension PaymentTitleExtension on String {
   // TODO(erdemyerebasmaz): Remove once default descriptions from the SDK is removed
   bool get isDefaultDescription {
-    return this == 'Send to L-BTC address' || this == 'Liquid transfer' || this == 'Lightning payment';
+    return this == 'Send to L-BTC address' ||
+        this == 'Liquid transfer' ||
+        this == 'Lightning payment' ||
+        this == 'Pay to Misty Breez';
   }
 }
