c8307557b35f2bf1f8dbb0aa51fe79b3f692e1c0
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-05-19 16:51:52 +0300

    feature: add option to paste clipboard items on restore flow

--- lib/routes/initial_walkthrough/mnemonics/enter_mnemonics_page.dart
+++ lib/routes/initial_walkthrough/mnemonics/enter_mnemonics_page.dart
@@ -1,8 +1,11 @@
 import 'package:breez_translations/breez_translations_locales.dart';
 import 'package:breez_translations/generated/breez_translations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:misty_breez/routes/routes.dart';
+import 'package:misty_breez/utils/constants/wordlist.dart';
 import 'package:misty_breez/widgets/back_button.dart' as back_button;
+import 'package:misty_breez/widgets/widgets.dart';
 
 class EnterMnemonicsPageArguments {
   final List<String> initialWords;
@@ -28,6 +31,9 @@ class EnterMnemonicsPage extends StatefulWidget {
 class EnterMnemonicsPageState extends State<EnterMnemonicsPage> {
   int _currentPage = 1;
   final int _lastPage = 2;
+
+  List<TextEditingController> textEditingControllers =
+      List<TextEditingController>.generate(12, (_) => TextEditingController());
 
   @override
   void initState() {
@@ -66,6 +72,17 @@ class EnterMnemonicsPageState extends State<EnterMnemonicsPage> {
               }
             },
           ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.content_paste_outlined,
+                color: Colors.white,
+                size: 20,
+              ),
+              tooltip: 'Paste Backup Phrase',
+              onPressed: _pasteBackupPhrase,
+            ),
+          ],
           title: Text(
             texts.enter_backup_phrase(
               _currentPage.toString(),
@@ -81,6 +98,7 @@ class EnterMnemonicsPageState extends State<EnterMnemonicsPage> {
               lastPage: _lastPage,
               initialWords: widget.arguments.initialWords,
               lastErrorMessage: widget.arguments.errorMessage,
+              textEditingControllers: textEditingControllers,
               changePage: () {
                 setState(() {
                   _currentPage++;
@@ -91,5 +109,42 @@ class EnterMnemonicsPageState extends State<EnterMnemonicsPage> {
         ),
       ),
     );
+  }
+
+  void _pasteBackupPhrase() async {
+    try {
+      final ClipboardData? clipboardData = await Clipboard.getData(Clipboard.kTextPlain);
+      final String? clipboardText = clipboardData?.text?.trim();
+
+      if (clipboardText == null) {
+        throw 'Clipboard is empty.';
+      }
+
+      final List<String> words = _extractWords(clipboardText);
+
+      if (_isValidMnemonic(words)) {
+        _populateTextFields(words);
+      } else {
+        throw 'Clipboard has invalid backup phrase.';
+      }
+    } catch (e) {
+      if (mounted) {
+        showFlushbar(context, message: e.toString());
+      }
+    }
+  }
+
+  List<String> _extractWords(String text) {
+    return text.split(RegExp(r'\s+')).map((String word) => word.toLowerCase().trim()).toList();
+  }
+
+  bool _isValidMnemonic(List<String> words) {
+    return words.length == 12 && words.every((String word) => wordlist.contains(word));
+  }
+
+  void _populateTextFields(List<String> words) {
+    for (int i = 0; i < words.length; i++) {
+      textEditingControllers[i].text = words[i];
+    }
   }
 }
