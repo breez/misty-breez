8633d29ce2b7c571eba148f5b6a3cb8d62fe966d
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2025-06-23 17:56:57 +0300

    feat: add a warning on BTC invoice page if there's been an error generating an amountless BTC address

--- lib/routes/receive_payment/widgets/payment_message_boxes/payment_message_boxes.dart
+++ lib/routes/receive_payment/widgets/payment_message_boxes/payment_message_boxes.dart
@@ -1,3 +1,4 @@
+export 'amountless_address_warning_box.dart';
 export 'ln_address_error_warning_box.dart';
 export 'notification_permission_warning_box.dart';
 export 'payment_fees_message_box.dart';
