48c3d612589d1627bb5363001bdf6908d66e4f4c
Erdem Yerebasmaz <erdem@yerebasmaz.com>
2026-02-09 17:38:05 +0300

    refactor: extract OptionalBlocFadeInRoute and NwcCubitFactory.of
    - Add generic OptionalBlocFadeInRoute to route.dart that reuses an
      existing cubit from context or wraps child in a BlocProvider
    - Add NwcCubitFactory.of convenience method to reduce boilerplate
    - Simplify NWC route cases in routes.dart using both

--- lib/cubit/nwc/factories/nwc_cubit_factory.dart
+++ lib/cubit/nwc/factories/nwc_cubit_factory.dart
@@ -1,9 +1,14 @@
 import 'package:breez_sdk_liquid/breez_sdk_liquid.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:misty_breez/cubit/cubit.dart';
 import 'package:misty_breez/utils/utils.dart';
 import 'package:service_injector/service_injector.dart';
 
 class NwcCubitFactory {
+  static NwcCubit of(BuildContext context) =>
+      create(ServiceInjector(), context.read<PermissionsCubit>());
+
   static NwcCubit create(ServiceInjector injector, PermissionsCubit permissionsCubit) {
     final BreezSDKLiquid breezSdkLiquid = injector.breezSdkLiquid;
     final MessageSigner messageSigner = MessageSigner(breezSdkLiquid);
